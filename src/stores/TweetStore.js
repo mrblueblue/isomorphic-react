72f485174e821faf6d761d449f2c8650c3964f73
Jonathan Huang <grokut@gmail.com>
2015-07-06 22:17:48 -0700

    fix/ link Tweet Store with Tweet Actions

--- src/stores/TweetStore.js
+++ src/stores/TweetStore.js
@@ -1,8 +1,28 @@
 import alt from '../alt';
+import TweetActions from '../actions/TweetActions';
 
 class TweetStore {
   constructor() {
     this.tweets = [];
+    this.errorMessage = null;
+    this.bindListeners({
+      handleUpdateTweets: TweetActions.UPDATE_TWEETS,
+      handleFetchTweets: TweetActions.FETCH_TWEETS,
+      handleTweetsFailed: TweetActions.MESSAGES_FAILED
+    });
+  }
+
+  handleUpdateTweets(tweets) {
+    this.tweets = tweets;
+    this.errorMessage = null;
+  }
+
+  handleFetchTweets() {
+    this.tweets = [];
+  }
+
+  handleTweetsFailed(errorMessage) {
+    this.errorMessage = errorMessage;
   }
 }
 
